28e53e4b9bb420bdd95e6a05e79b221e5db86c6a
Zhou Bowei <zhoubw12@mails.tsinghua.edu.cn>
2016-04-08 20:22:24 +0800

    small update

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function randomInts(range) {
 	var res = [];
 	while (res.length < range) {
 		var num = Math.ceil(Math.random() * range);
-        // console.log(num);
+		// console.log(num);
 		if (res.indexOf(num) >= 0 || res.length + 1 == num) {
 			continue;
 		}
@@ -17,8 +17,8 @@ function randomInts(range) {
 
 function initGame1() {
 	loadCount = GAME1_SIZE * GAME1_SIZE + 2;
-    $('#game_section1 #img_area')[0].innerHTML = '<img src="img/Lenna.png" width=100% onload="loadCount--;checkLoad(loadCount, 1);">'
-	$('#game1_cache').append('<img class="game1_imgback" src="img/kuma.jpg" width=100% onload="loadCount--;checkLoad(loadCount, 1);">');
+	$('#game_section1 #img_area')[0].innerHTML = '<img src="img/Lenna.png" width=100% onload="loadCount--;checkLoad(loadCount, 1);">'
+	$('#game1_cache').append('<img class="game1_imgback" src="img/doge.png" width=100% onload="loadCount--;checkLoad(loadCount, 1);">');
 	var item = $('#game_section1 table');
 	// item[0].innerHTML = '';
 
@@ -27,16 +27,16 @@ function initGame1() {
 		text += '<tr>';
 		for (var j = 0; j < GAME1_SIZE; j++) {
 			var idx = i * GAME1_SIZE + j + 1;
-			text += '<td><div id="game1_img'+idx+'_container"><img id="game1_img'+idx+'" src="img/Lenna_0' + idx + '.png" width=100% onload="loadCount--;checkLoad(loadCount, 1);"></div></td>';
+			text += '<td><div id="game1_img' + idx + '_container"><img id="game1_img' + idx + '" src="img/Lenna_0' + idx + '.png" width=100% onload="loadCount--;checkLoad(loadCount, 1);"></div></td>';
 		}
 		text += '</tr>';
 	}
 
 	item[0].innerHTML = text;
-    game1_img_list = [];
-    for (var i=0;i<GAME1_SIZE*GAME1_SIZE;i++){
-        game1_img_list.push($('#game1_img'+(i+1)));
-    }
+	game1_img_list = [];
+	for (var i = 0; i < GAME1_SIZE * GAME1_SIZE; i++) {
+		game1_img_list.push($('#game1_img' + (i + 1)));
+	}
 
 	// var cell = $('#game_section1 td');
 	// $('#game_section_area ')
@@ -60,15 +60,52 @@ function game1Random() {
 	// console.log(1);
 	// $('td').css('padding','0.8%');
 	$('#game_section1 .mask').hide();
-    // TODO: 这里还要加上一些动画OMG...
-	var ints = randomInts(GAME1_SIZE * GAME1_SIZE);
-    for (var i = 0; i < GAME1_SIZE; i++) {
-        for (var j = 0; j < GAME1_SIZE; j++) {
-            var idx = i * GAME1_SIZE + j + 1;
-            $('#game1_img'+idx+'_container').html(game1_img_list[ints[idx-1]-1]);
-        }
-    }
+	// TODO: 这里还要加上一些动画OMG...
+	// UPDATE: 动画来了
+
+	$('td div').html($('.game1_imgback'));
+	// $('td div').each(function(i, e) {
+	//     console.log(e);
+	//     $(e).css('transform', 'rotateX('+Math.random()*360+')')
+	// });
+
+    animate(3);
+
+	setTimeout(function() {
+		var ints = randomInts(GAME1_SIZE * GAME1_SIZE);
+		for (var i = 0; i < GAME1_SIZE; i++) {
+			for (var j = 0; j < GAME1_SIZE; j++) {
+				var idx = i * GAME1_SIZE + j + 1;
+				$('#game1_img' + idx + '_container').html(game1_img_list[ints[idx - 1] - 1]);
+                console.log(idx);
+			}
+		}
+	}, 1600);
+
 	// $('td div img').css('opacity', '0');
+}
+
+function animate(count) {
+    console.log('ani',count);
+    if (count<0){
+        return;
+    }
+    if (count==0){
+        setTimeout(function(){
+            for (var i = 0; i < GAME1_SIZE * GAME1_SIZE; i++) {
+                $('#game1_img' + (i + 1) + '_container')[0].children[0].style.transform =
+                    'rotate(' + 0 + 'deg)';
+            }
+        },400);
+        return;
+    }
+    setTimeout(function(){
+        for (var i = 0; i < GAME1_SIZE * GAME1_SIZE; i++) {
+            $('#game1_img' + (i + 1) + '_container')[0].children[0].style.transform =
+                'rotate(' + (Math.random() > 0.5 ? '+' : '-') + Math.random() * 360 + 'deg)';
+        }
+        animate(count-1);
+    },400);
 }
 
 function countdown(item, time, foo) {
