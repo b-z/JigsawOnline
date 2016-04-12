411cb829e6cec059dcc165755f6ac9742e526b29
Zhou Bowei <zhoubw12@mails.tsinghua.edu.cn>
2016-04-12 16:57:42 +0800

    add game3

--- js/main.js
+++ js/main.js
@@ -4,14 +4,20 @@ var GAME3_SIZE = 5;
 var game1_img_list = [];
 var game2_img_list = [];
 var game3_img_list = [];
+var game3_list = [];
 var flag_can_move = false;
 var flag_on_touch = false;
 var stage = 1;
-var loadCount = GAME1_SIZE * GAME1_SIZE + GAME2_SIZE * GAME2_SIZE + 2;
+var loadCount = GAME1_SIZE * GAME1_SIZE + GAME2_SIZE * GAME2_SIZE + GAME3_SIZE * GAME3_SIZE + 3;
 var loadCountTotal = loadCount;
 var game2_random_move_count = 100;
 var game2_last_choose = 0;
-var touch_start_point={x:0,y:0};
+var game3_flip_count = GAME3_SIZE * GAME3_SIZE;
+var game3_word = '公益文化';
+var touch_start_point = {
+	x: 0,
+	y: 0
+};
 
 function randomInts(range) {
 	// 产生[1，range]的随机整数
@@ -27,8 +33,9 @@ function randomInts(range) {
 }
 
 function loadImage() {
-	$('#game_section1 #img_area')[0].innerHTML = '<img src="img/Lenna.png" width=100% onload="loadCount--;checkLoad(loadCount, 1);">'
-	$('#game_cache').append('<img class="game1_imgback" src="img/doge.png" width=100% onload="loadCount--;checkLoad(loadCount, 1);">');
+	$('#game_section1 #img_area').html('<img src="img/Lenna.png" width=100% onload="loadCount--;checkLoad(loadCount, 1);">');
+	$('#game_cache').append('<img class="game1_imgback" src="img/back2.jpg" width=100% onload="loadCount--;checkLoad(loadCount, 1);">');
+	$('#game_cache').append('<img class="game3_imgback" src="img/back.jpg" width=100% onload="loadCount--;checkLoad(loadCount, 1);">');
 
 	var text = '';
 	for (var i = 0; i < GAME1_SIZE; i++) {
@@ -39,7 +46,7 @@ function loadImage() {
 		}
 		text += '</tr>';
 	}
-	$('#game_section1 table')[0].innerHTML = text;
+	$('#game_section1 table').html(text);
 	game1_img_list = [];
 	for (var i = 0; i < GAME1_SIZE * GAME1_SIZE; i++) {
 		game1_img_list.push($('#game1_img' + (i + 1)));
@@ -54,21 +61,43 @@ function loadImage() {
 		}
 		text += '</tr>';
 	}
-	$('#game_section2 table')[0].innerHTML = text;
+	$('#game_section2 table').html(text);
 	game2_img_list = [];
 	for (var i = 0; i < GAME2_SIZE * GAME2_SIZE; i++) {
 		game2_img_list.push($('#game2_img' + (i + 1)));
+	}
+
+	text = '';
+	var ints = randomInts(GAME3_SIZE * GAME3_SIZE);
+	for (var i = 0; i < GAME3_SIZE; i++) {
+		text += '<tr>';
+		for (var j = 0; j < GAME3_SIZE; j++) {
+			var idx = i * GAME3_SIZE + j + 1;
+			var pos = ints.indexOf(idx);
+			var name = '随机' [Math.floor(2 * Math.random())];
+			if (pos < 4) {
+				name = game3_word[pos];
+			}
+			game3_list.push(name);
+			text += '<td><div id="game3_img' + idx + '_container"><img alt="' + name + '" id="game3_img' + idx + '" src="img/' + name + '.png" width=100% onload="loadCount--;checkLoad(loadCount, 1);"></div></td>';
+		}
+		text += '</tr>';
+	}
+	$('#game_section3 table').html(text);
+	game3_img_list = [];
+	for (var i = 0; i < GAME3_SIZE * GAME3_SIZE; i++) {
+		game3_img_list.push($('#game3_img' + (i + 1)));
 	}
 }
 
 function checkLoad(count, section) {
 	console.log(count);
 	var txt = '99.';
-	for (var i = 0; i < loadCountTotal-count; i++) {
+	for (var i = 0; i < loadCountTotal - count; i += 3) {
 		txt += '9';
 	}
-    txt+='%';
-	$('#loading span')[0].innerHTML = txt;
+	txt += '%';
+	$('#loading span').html(txt);
 	if (count == 0) {
 		$('#loading').hide();
 		// initGame1();
@@ -124,6 +153,81 @@ function game2Random() {
 	exchangeFast(container1, container2, game2Random);
 }
 
+function game3Random() {
+	for (var i = 0; i < GAME3_SIZE * GAME3_SIZE; i++) {
+		game3RandomFlip(i + 1);
+	}
+}
+
+function game3RandomFlip(idx) {
+	setTimeout(function() {
+		flip($('#game3_img' + idx + '_container'), idx, function() {
+			game3_flip_count--;
+			if (game3_flip_count == 0) {
+				game3Start();
+			}
+		});
+	}, 3000 + Math.random() * 1000);
+}
+
+function game3Start() {
+	flag_can_move = true;
+}
+
+function game3Click(idx) {
+	var status = game3Status(idx);
+	console.log(status);
+	if (game3_word.indexOf(status) >= 0) {
+		return;
+	}
+	var container = $('#game3_img' + idx + '_container');
+	flip(container, idx, function() {
+		if (game3_word.indexOf(game3_list[idx - 1]) < 0) {
+			flag_can_move = false;
+			setTimeout(function() {
+				flip(container, idx, function() {
+					var idx1, idx2;
+					while (true) {
+						idx1 = Math.floor(Math.random() * GAME3_SIZE * GAME3_SIZE) + 1;
+						idx2 = Math.floor(Math.random() * GAME3_SIZE * GAME3_SIZE) + 1;
+						if (idx1 != idx2 && game3Status(idx1) == 'back' && game3Status(idx2) == 'back') {
+							break;
+						}
+					}
+					var container1 = $('#game3_img' + idx1 + '_container');
+					var container2 = $('#game3_img' + idx2 + '_container');
+					var t = game3_list[idx1-1];
+					game3_list[idx1-1] = game3_list[idx2-1];
+					game3_list[idx2-1] = t;
+					exchange(container1, container2, function() {
+						flag_can_move = true;
+					})
+				});
+			}, 1000);
+		} else {
+			game3Check();
+		}
+	});
+}
+
+function game3Check() {
+	var c = 0;
+	for (var i=1;i<=GAME3_SIZE*GAME3_SIZE;i++){
+		if (game3_word.indexOf(game3Status(i))>=0){
+			c++;
+		}
+	}
+	if (c==game3_word.length){
+		game3Finish();
+	}
+}
+
+function game3Status(idx) {
+	var container = $('#game3_img' + idx + '_container');
+	var image_now = container[0].children[0].alt;
+	return image_now;
+}
+
 function game2Move(direction) {
 	var empty_id = GAME2_SIZE * GAME2_SIZE;
 	var container1 = $('#game2_img' + empty_id).parent();
@@ -148,7 +252,7 @@ function game2Move(direction) {
 	}
 	var container2 = $('#game2_img' + idx2 + '_container');
 	exchange(container1, container2, game2Check);
-    return true;
+	return true;
 }
 
 function game2Check() {
@@ -220,7 +324,7 @@ function countdown(item, time, foo) {
 	if (time <= 0) {
 		foo();
 	} else {
-		item[0].innerHTML = '' + time;
+		item.html('' + time);
 		item.fadeIn(500);
 		setTimeout(function() {
 			item.hide();
@@ -269,6 +373,29 @@ function exchange(container1, container2, foo) {
 	// }, 400);
 }
 
+function flip(container, idx, foo) {
+	var temp = flag_can_move;
+	flag_can_move = false;
+	var axis = (Math.random() > 0.5 ? 'X' : 'Y');
+	container[0].children[0].style.transform = 'rotate' + axis + '(90deg)';
+
+	var image_now = game3Status(idx);
+	var image = 'back.jpg';
+	if (image_now == 'back') {
+		image = game3_list[idx - 1] + '.png';
+	}
+	setTimeout(function() {
+		container.html('<img alt="' + image.split('.')[0] + '" src="img/' + image + '" width=100% style="transform:rotate' + axis + '(90deg);">');
+		setTimeout(function() {
+			container[0].children[0].style.transform = 'rotate' + axis + '(0deg)';
+		}, 10);
+	}, 200);
+	setTimeout(function() {
+		flag_can_move = temp;
+		foo();
+	}, 410);
+}
+
 function game1Check() {
 	var imgs = $('#game_section1 td').children().children();
 	var result = true;
@@ -304,8 +431,13 @@ function game2Finish() {
 	$('#game_section2').hide();
 	$('#game_section3').show();
 	$('#game_section3 .mask').hide();
+	game3Random();
+}
 
-
+function game3Finish() {
+	flag_on_touch = false;
+	flag_can_move = false;
+	alert('yeah!');
 }
 
 function setHoverStyle(elements, remove) {
@@ -320,7 +452,7 @@ function setHoverStyle(elements, remove) {
 }
 
 function ontouchstart(e) {
-    touch_start_point.x = e.changedTouches[e.changedTouches.length - 1].clientX;
+	touch_start_point.x = e.changedTouches[e.changedTouches.length - 1].clientX;
 	touch_start_point.y = e.changedTouches[e.changedTouches.length - 1].clientY;
 	if (!flag_can_move) {
 		return;
@@ -335,6 +467,12 @@ function ontouchstart(e) {
 			flags.push($('#game_section1 td img')[i] != e.srcElement);
 		}
 		setHoverStyle($('#game_section1 td img'), flags);
+	} else if (stage == 3) {
+		var flags = [];
+		for (var i = 0; i < GAME3_SIZE * GAME3_SIZE; i++) {
+			flags.push($('#game_section3 td img')[i] != e.srcElement);
+		}
+		setHoverStyle($('#game_section3 td img'), flags);
 	}
 	// console.log('start',e)
 }
@@ -353,7 +491,7 @@ function ontouchmove(e) {
 	// console.log(e.target);
 	var x = e.changedTouches[e.changedTouches.length - 1].clientX;
 	var y = e.changedTouches[e.changedTouches.length - 1].clientY;
-    var dx = x - touch_start_point.x;
+	var dx = x - touch_start_point.x;
 	var dy = y - touch_start_point.y;
 	var source = e.srcElement;
 	var target = document.elementFromPoint(x, y);
@@ -365,10 +503,10 @@ function ontouchmove(e) {
 			flags.push($('#game_section1 td img')[i] != source && $('#game_section1 td img')[i] != target);
 		}
 		setHoverStyle($('#game_section1 td img'), flags);
-	}else if (stage==2) {
-        if (dx * dx + dy * dy > 3000) {
+	} else if (stage == 2) {
+		if (dx * dx + dy * dy > 3000) {
 			// 滑动
-            flag_on_touch = false;
+			flag_on_touch = false;
 			if (dx > dy) {
 				if (dx > -dy) {
 					game2Move('right');
@@ -388,7 +526,7 @@ function ontouchmove(e) {
 }
 
 function ontouchend(e) {
-    // console.log(e);
+	// console.log(e);
 	if (!flag_on_touch) {
 		return;
 	}
@@ -415,21 +553,33 @@ function ontouchend(e) {
 		}
 
 		exchange($(source).parent(), $(target).parent(), game1Check);
+	} else if (stage == 3) {
+		var flags = [];
+		for (var i = 0; i < GAME3_SIZE * GAME3_SIZE; i++) {
+			flags.push(true);
+		}
+		setHoverStyle($('#game_section3 td img'), flags);
+		if (source != target || target.tagName != 'IMG') {
+			return;
+		}
+		var container = $(target).parent();
+		var idx = container.attr('id').split('img')[1].split('_')[0] - 0;
+		game3Click(idx);
 	} else if (stage == 2) {
 		// console.log(dx * dx + dy * dy);
 		if (dx * dx + dy * dy < 1000) {
 			// 点击
-            var empty_id = GAME2_SIZE * GAME2_SIZE;
-        	var container1 = $('#game2_img' + empty_id).parent();
-        	var idx1 = container1.attr('id').split('img')[1].split('_')[0] - 0;
-        	var idx2 = $(target).parent().attr('id').split('img')[1].split('_')[0] - 0;
-            if (!game2CheckValid(idx1, idx2)) {
-        		return false;
-        	}
-        	var container2 = $('#game2_img' + idx2 + '_container');
-        	exchange(container1, container2, game2Check);
+			var empty_id = GAME2_SIZE * GAME2_SIZE;
+			var container1 = $('#game2_img' + empty_id).parent();
+			var idx1 = container1.attr('id').split('img')[1].split('_')[0] - 0;
+			var idx2 = $(target).parent().attr('id').split('img')[1].split('_')[0] - 0;
+			if (!game2CheckValid(idx1, idx2)) {
+				return false;
+			}
+			var container2 = $('#game2_img' + idx2 + '_container');
+			exchange(container1, container2, game2Check);
 		} else {
-            if (dx > dy) {
+			if (dx > dy) {
 				if (dx > -dy) {
 					game2Move('right');
 				} else {
@@ -442,7 +592,7 @@ function ontouchend(e) {
 					game2Move('left');
 				}
 			}
-        }
+		}
 	}
 
 	// console.log('end', e);
@@ -451,12 +601,16 @@ function ontouchend(e) {
 function onkeydown(e) {
 	// console.log(e);
 	if (e.keyCode == 32) {
-		game1Finish();
+		if (stage == 1) {
+			game1Finish();
+		} else if (stage == 2) {
+			game2Finish();
+		}
 	}
 	if (stage == 2) {
-        if (!flag_can_move){
-            return;
-        }
+		if (!flag_can_move) {
+			return;
+		}
 		if (e.keyCode == 38) {
 			game2Move('up');
 		}
